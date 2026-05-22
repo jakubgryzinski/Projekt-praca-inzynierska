f9e366cce917472f9ff7627eb9165b2a6c6c20f9
Jakub Gryziński <jakub.gryzinski+github@gmail.com>
2026-05-22 13:34:57 +0200

    Frontend app setup lint and type check

---
+++ README.md
@@ -0,0 +1,29 @@
+# Projekt praca inżynierska
+
+## Frontend
+
+```
+cd apps/frontend
+```
+
+```
+cp .env.example .env.local
+```
+
+```
+npm i
+```
+
+```
+npm run dev
+```
+
+```
+npm run lint
+```
+
+```
+npm run typecheck
+```
+
+## Backend
--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -2,11 +2,27 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier/flat'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import importPlugin from 'eslint-plugin-import'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'public', '**/.svgr.cjs', '**/routeTree.gen.ts']),
+  importPlugin.flatConfigs.recommended,
+  {
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': {
+        typescript: {},
+        node: { extensions: ['*'] },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+  prettier,
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -14,9 +30,46 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
     ],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'react-hooks/exhaustive-deps': ['error'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'react/jsx-key': ['error', { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true }],
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      'no-console': ['error', { allow: ['info', 'warn', 'error', 'debug'] }],
+      'arrow-body-style': ['error', 'as-needed'],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never', js: 'never', jsx: 'never' }],
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: '*', next: 'if' },
+        { blankLine: 'always', prev: 'if', next: '*' },
+      ],
     },
   },
 ])
